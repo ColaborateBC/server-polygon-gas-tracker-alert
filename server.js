15fc3266bb98f6c25c110aecef6446c9e885ce00
leofab <leofabr3@gmail.com>
2023-04-30 17:10:09 -0300

    telegram bot implementing still need to work on the message handling

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const request = require("axios");
 const bodyParser = require("body-parser");
 const { default: axios } = require("axios");
 const config = require("./config"); // Load the exported data from config.js
+const TelegramBot = require("node-telegram-bot-api");
+
 
 const API_KEY = config.API_KEY;
+const TEL_API_KEY = config.TEL_API_KEY;
 
 const app = express();
 
 app.use(bodyParser.json());
+
+const bot = new TelegramBot(TEL_API_KEY, { polling: false });
+
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // Update this with the appropriate origin URL for production use
@@ -20,6 +26,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Content-Type, Authorization, Content-Length, X-Requested-With"
   );
+  res.header("Content-Type", "application/javascript");
   next();
 });
 
@@ -42,6 +49,35 @@ app.get("/get-data", (req, res) => {
     .catch(error => {
       res.status(500).send("Error");
     });
+});
+
+app.post("/send-message", (req, res) => {
+  const gasPriceThreshold = req.body.gasPriceThreshold;
+  const phoneNumber = req.body.phoneNumber;
+  const apiUrl =
+    "https://api.polygonscan.com/api?module=gastracker&action=gasoracle&apikey=" +
+    API_KEY; // Construct the API URL with the stored API key
+  axios.get(apiUrl)
+    .then(response => {
+      const data = response.data;
+      const proposeGasPrice = data.result.ProposeGasPrice;
+      const fastGasPrice = data.result.FastGasPrice;
+      // Update the response with the relevant data
+      const responseJson = {
+        proposeGasPrice: proposeGasPrice,
+        fastGasPrice: fastGasPrice,
+      };
+      if (proposeGasPrice <= gasPriceThreshold || fastGasPrice <= gasPriceThreshold) {
+        const message = `Gas price has dropped below ${gasPriceThreshold}. Current prices: Proposed: ${proposeGasPrice}, Fast: ${fastGasPrice}`;
+        bot.sendMessage(phoneNumber, message);
+      }
+    })
+    .catch(error => {
+      res.status(500).send("Error");
+    });
+
+  res.json({ success: true });
+
 });
 
 app.listen(3000, () => {
