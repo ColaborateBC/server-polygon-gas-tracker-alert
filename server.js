29f6cd0425eef279a3255c2f40bcb249b6758618
leofab <leofabr3@gmail.com>
2023-05-01 14:32:27 -0300

    resolving bot sendMessage error while importing bot from other file

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-import bot from "./config";
 const express = require("express");
-const request = require("axios");
 const bodyParser = require("body-parser");
 const { default: axios } = require("axios");
 const config = require("./config"); // Load the exported data from config.js
-
+const TelegramBot = require("node-telegram-bot-api");
 
 const API_KEY = config.API_KEY;
+let gasPrices = {};
 
 const app = express();
 
 app.use(bodyParser.json());
 
+const bot = new TelegramBot(config.TEL_API_KEY || [YOUR_BOT_API_TOKEN], { polling: true });
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // Update this with the appropriate origin URL for production use
@@ -41,36 +41,26 @@ app.get("/get-data", (req, res) => {
         proposeGasPrice: proposeGasPrice,
         fastGasPrice: fastGasPrice,
       };
+      gasPrices = {
+        proposeGasPrice: proposeGasPrice,
+        fastGasPrice: fastGasPrice,
+      };
       res.json(responseJson);
     })
-    .catch(error => {
-      res.status(500).send("Error");
-    });
+    // .catch(error => {
+    //   res.status(500).send("Error");
+    // });
 });
 
 app.post("/", (req, res) => {
   const gasPriceThreshold = req.body.gasPriceThreshold;
   const telId = req.body.telId;
-  const apiUrl =
-    "https://api.polygonscan.com/api?module=gastracker&action=gasoracle&apikey=" +
-    API_KEY; // Construct the API URL with the stored API key
-
-  axios.get(apiUrl)
-    .then(response => {
-      const data = response.data;
-      const proposeGasPrice = data.result.ProposeGasPrice;
-      const fastGasPrice = data.result.FastGasPrice;
-
-    })
-    .catch(error => {
-      res.status(500).send("Error");
-    });
 
   // res.json({ success: true });
-  if (proposeGasPrice <= gasPriceThreshold || fastGasPrice <= gasPriceThreshold) {
-    const message = `Gas price has dropped below ${gasPriceThreshold}. Current prices: Proposed: ${proposeGasPrice}, Fast: ${fastGasPrice}`;
-    console.log(message);
-    bot.sendMessage(telId, message);
+  if (gasPrices.proposeGasPrice <= gasPriceThreshold || gasPrices.fastGasPrice <= gasPriceThreshold) {
+    const message = `Gas price has dropped below ${gasPriceThreshold}. Current prices: Proposed: ${gasPrices.proposeGasPrice}, Fast: ${gasPrices.fastGasPrice}`;
+    // console.log(message);
+    bot.sendMessage("-852511629", message)
   }
 });
 
